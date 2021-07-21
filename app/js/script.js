2040c3a98ff3098396c61b0f3d864afb4611b07d
Szymon Szymański <szymon.szymanski@performance-media.pl>
2021-07-22 01:35:10 +0200

    first version of filter products, it suck

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,25 @@
 'use strict';
+
+const filters = document.querySelectorAll('.filter-box__option');
+
+filters.forEach((filter) => {
+  filter.addEventListener('click', function () {
+    let selectedFilter = filter.getAttribute('data-filter');
+
+    let itemsToShow = document.querySelectorAll(
+      `.products__container [data-filter='${selectedFilter}']`
+    );
+    let itemsToHide = document.querySelectorAll(
+      `.products__product:not([data-filter='${selectedFilter}']), 
+      .filter-box__option:not([data-filter='${selectedFilter}'])`
+    );
+
+    itemsToHide.forEach((el) => {
+      el.classList.remove('active');
+    });
+
+    itemsToShow.forEach((el) => {
+      el.classList.add('active');
+    });
+  });
+});
